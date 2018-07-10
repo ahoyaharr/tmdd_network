61be3a8b1ad7ee3f64f5bf646e5d76688f90cedb
AhoyAharr <alexanderharrison1994@gmail.com>
2018-07-10 16:29:38 -0700

    network coordinate extractor inital commit

---
+++ tmdd_network_coordinate_extractor.py
@@ -0,0 +1,61 @@
+import os
+import sys
+
+
+def separator():
+    UNIX_ENCODING = '/'
+    return UNIX_ENCODING
+
+def get_script_path(p):
+    return os.path.dirname(os.path.realpath(sys.argv[0])) + separator() + p
+
+def get_JSON_files(path='data', absolute=False):
+    # Returns a list of paths to uncorrected files in a directory.
+    # path: the path to the directory
+    # absolute: whether or not the filePath should be relative, i.e. ~/myFile.file vs. ~/.../myFile.file
+    # system_type: 'windows' or 'unix'
+    files = [file for file in os.listdir(get_script_path(path)) if '.json' in file]
+    return [get_script_path(path) + separator() + file for file in files] if absolute else files
+
+def read_file(s, dir='data'):
+    file = open(os.path.dirname(os.path.realpath(sys.argv[0])) + separator() + dir + separator() + s)
+    return file.read()
+
+def get_JSON_strings():
+    """
+    Maps the file name without the extension to the associated JSON string.
+    """
+    return {file.rsplit(".", 1)[0]: read_file(file) for file in get_JSON_files()}
+
+def export(header, data, filename):
+    """
+    Write a CSV as defined by the header, and the dictionaries holding data to the ~/exports/ directory.
+    :param header: A sequence of strings of length k, where each item is a column name
+    :param data: A sequence of tuples each of length k, where each item in a row in the relation.
+    :return: True if successful, otherwise False
+    """
+    assert type(data) is list and len(data) > 0  # Data must be a list containing data.
+    assert type(header) is list and len(header) > 0  # Header must be a list containing data.
+
+    for row in data:
+        # Each row must be a dictionary, and the keys of the dictionary must be aligned with the header.
+        assert len(header) == len(row.keys()) and type(row) is dict
+
+    filepath = p.get_script_path('exports') + p.separator() + filename + '.csv'
+    with open(filepath, 'w', newline='\n') as csvfile:
+        f = csv.DictWriter(csvfile, fieldnames=header, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+        f.writeheader()
+        for row in data:
+            f.writerow(row)
+
+
+tmdd_object_system = json.loads(json_strings['tmdd'])
+link_inventory = tmdd_object_system['LinkInventory']
+
+data = []
+for count, link in enumerate(link_inventory):
+	for point in link['link-geom-location']:
+		data.append({'lon': point['longitude'], 'lat': point['latitude'], 'id': count})
+
+export(['lat','lon','id'], data, 'tmdd')
