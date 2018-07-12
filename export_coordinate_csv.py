5fbc6ff3f8bd4765dfae6f250d28c3c0a999b76e
AhoyAharr <alexanderharrison1994@gmail.com>
2018-07-12 15:49:41 -0700

    export all valid files in data dir

--- export_coordinate_csv.py
+++ export_coordinate_csv.py
@@ -1,14 +1,18 @@
 import json
-
 import local_io as io
 
-tmdd_object_system = json.loads(io.get_JSON_strings()['tmdd'])
+""" extracts the coordinates of each .json file in the ~/tmdd_network/data/ directory. """
 
-link_inventory = tmdd_object_system['LinkInventory']['link-inventory-list']
+data_files = io.get_JSON_strings()
 
-""" build and write the .csv for visualising the network """
-data = []
-for count, link in enumerate(link_inventory):
-    for point in link['link-geom-location']:
-        data.append({'lon': point['longitude'], 'lat': point['latitude'], 'id': count})
-io.export(['lat', 'lon', 'id'], data, 'tmdd')
+for key in data_files.keys():
+	print('processing {0}.json'.format(key))
+	tmdd_object_system = json.loads(data_files[key])
+	link_inventory = tmdd_object_system['LinkInventory']['link-inventory-list']
+
+	""" build and write the .csv for visualising the network """
+	data = []
+	for count, link in enumerate(link_inventory):
+	    for point in link['link-geom-location']:
+	        data.append({'lon': point['longitude'], 'lat': point['latitude'], 'id': count})
+	io.export(['lat', 'lon', 'id'], data, key)
